078452af475f1eee8057512bf361f248e8c46b8f
Hunter Eckhoff <Phantazm777@verizon.net>
2023-05-11 17:25:48 -0400

    setting up trivia controller and trivia routes framework, and importing and mounting in server.js

---
+++ routes/trivia.js
@@ -0,0 +1,14 @@
+import { Router } from 'express'
+import * as triviaCtrl from '../controllers/trivia.js'
+import { decodeUserFromToken, checkAuth } from '../middleware/auth.js'
+
+const router = Router()
+
+// ========== Public Routes ===========
+
+
+// ========= Protected Routes ========= 
+router.use(decodeUserFromToken)
+
+
+export { router }
