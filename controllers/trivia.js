55725ba748df7a63f6dfd5095e479d65715ffd4b
Jerry Pao <kyjerry0424@gmail.com>
2023-05-12 16:56:40 -0400

    define create controller function

--- controllers/trivia.js
+++ controllers/trivia.js
@@ -1,5 +1,23 @@
 import { Profile } from "../models/profile.js"
 import { Trivia } from "../models/trivia.js"
 
+const create = async (req, res) => {
+  try {
+    req.body.author = req.user.profile
+    const trivia = await Trivia.create(req.body)
+    const profile = await Profile.findByIdAndUpdate(
+      req.user.profile,
+      { $push: { trivia: trivia } },
+      { new: true }
+    )
+    trivia.owner = profile
+    res.status(201).json(blog);
+  } catch (err) {
+    console.log(err)
+    res.status(500).json(err)
+  }
+}
 
-export { }
+export {
+    create,
+}
