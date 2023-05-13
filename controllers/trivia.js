88ce6f5a4db440fa7dc5e45ff0749d2154bdf15c
andrewwinke123 <andrewwinke@gmail.com>
2023-05-13 01:59:49 -0400

    finishing out trivia  create functionality

--- controllers/trivia.js
+++ controllers/trivia.js
@@ -2,4 +2,23 @@ import { Profile } from "../models/profile.js"
 import { Trivia } from "../models/trivia.js"
 
 
-export { }
+async function create(req, res) {
+  try {
+    req.body.author = req.user.profile
+    const trivia = await Trivia.create(req.body)
+    const profile = await Profile.findByIdAndUpdate(
+      req.user.profile,
+      { $push: { trivias: trivia } },
+      { new: true }
+    )
+    trivia.author = profile
+    res.status(201).json(trivia)
+  } catch (error) {
+    console.log(error)
+    res.status(500).json(error)
+  }
+}
+
+export {
+  create,
+}
