7fb2cec11ba6fa99a83209a1ff4cd036ade575cc
Jerry Pao <kyjerry0424@gmail.com>
2023-05-13 13:47:34 -0400

    fix the create controller function

--- controllers/trivia.js
+++ controllers/trivia.js
@@ -4,7 +4,23 @@ import { Trivia } from "../models/trivia.js"
 const create = async (req, res) => {
   try {
     req.body.owner = req.user.profile
-    console.log(req.body)
+    req.body.questions = [{
+        "text": req.body.question,
+        "choices": [{
+            "text": req.body.answer1,
+            "answer": req.body.checkbox1
+        },{
+            "text": req.body.answer2,
+            "answer": req.body.checkbox2
+        },{
+            "text": req.body.answer3,
+            "answer": req.body.checkbox3
+        },{
+            "text": req.body.answer4,
+            "answer": req.body.checkbox4
+        }]
+    }]
+    console.log("req.body ==> ",req.body)
     const trivia = await Trivia.create(req.body)
     const profile = await Profile.findByIdAndUpdate(
       req.user.profile,
