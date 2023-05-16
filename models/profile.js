515cf62f3452b81d49ce5cbbc564e3b1801c70c0
Jerry Pao <kyjerry0424@gmail.com>
2023-05-16 10:28:17 -0400

    define add score route and controller function, revise trivia and profile schema

--- models/profile.js
+++ models/profile.js
@@ -2,11 +2,18 @@ import mongoose from 'mongoose'
 
 const Schema = mongoose.Schema
 
+const scoreSchema = new Schema({
+  score: Number,
+  trivia: {type: Schema.Types.ObjectId, ref: "Score"}
+},{
+  timestamps: true,
+})
+
 const profileSchema = new Schema({
   name: String,
   photo: String,
   trivia: [{type: Schema.Types.ObjectId, ref: "Trivia" }],
-  scores: [{type: Schema.Types.ObjectId, ref: "Score"}]
+  scores: [scoreSchema]
 },{
   timestamps: true,
 })
