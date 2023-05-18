f77f54a8187d8ea5ab79e328c81b183698ad66e8
amirb <amir.bullock9@gmail.com>
2023-05-18 00:24:08 -0400

    create route and export for show function

--- controllers/profiles.js
+++ controllers/profiles.js
@@ -1,5 +1,5 @@
-import { Profile } from '../models/profile.js'
-import { v2 as cloudinary } from 'cloudinary'
+import { Profile } from "../models/profile.js"
+import { v2 as cloudinary } from "cloudinary"
 
 async function index(req, res) {
   try {
@@ -15,11 +15,11 @@ async function show(req, res) {
   try {
     const profileId = req.params.profileId
     const profile = await Profile.findById(profileId)
-    
+
     if (!profile) {
-      return res.status(404).json({ message: 'Profile not found' })
+      return res.status(404).json({ message: "Profile not found" })
     }
-    
+
     res.json(profile)
   } catch (err) {
     console.log(err)
@@ -32,12 +32,11 @@ async function addPhoto(req, res) {
     const imageFile = req.files.photo.path
     const profile = await Profile.findById(req.params.id)
 
-    const image = await cloudinary.uploader.upload(
-      imageFile, 
-      { tags: `${req.user.email}` }
-    )
+    const image = await cloudinary.uploader.upload(imageFile, {
+      tags: `${req.user.email}`,
+    })
     profile.photo = image.url
-    
+
     await profile.save()
     res.status(201).json(profile.photo)
   } catch (err) {
@@ -46,4 +45,4 @@ async function addPhoto(req, res) {
   }
 }
 
-export { index, addPhoto }
+export { index, addPhoto, show }
